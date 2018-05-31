756384a0a14f58c52db2de0a7959515968fab4b9
EanKeen <24364012+EanKeen@users.noreply.github.com>
2018-05-30 23:46:42 -0700

    Initial Commit

---
+++ Control.java
@@ -0,0 +1,6 @@
+public class Control {
+  public static void main(String[] args) {
+    System.out.println("Test");
+    Interact.output("Test 2");
+  }
+}
---
+++ Interact.java
@@ -0,0 +1,7 @@
+public class Interact {
+  // interact class interacts with the user
+  public static void output(String output)
+  {
+    System.out.println(output);
+  }
+}
